bec1c6eabfed09183437df7840f193d910873d95
Chris Zimmerman <chris@chriszimmerman.net>
2025-06-04 15:48:03 -0400

    Style solution page so that subgrids are more distinguishable

--- app/helpers/sudoku_helper.rb
+++ app/helpers/sudoku_helper.rb
@@ -1,2 +1,17 @@
 module SudokuHelper
+  def solution_classes(index)
+    base_classes = "w-10 text-2xl text-center border"
+    right_border_indexes = [2, 5, 11, 14, 20, 23, 29, 32, 38, 41, 47, 50, 56, 59, 65, 68, 74, 77]
+    bottom_border_indexes = [18, 19, 20, 21, 22, 23, 24, 25, 26, 45, 46, 47, 48, 49, 50, 51, 52, 53]
+
+    if right_border_indexes.include?(index)
+      base_classes.concat(" border-r-4")
+    end
+
+    if bottom_border_indexes.include?(index)
+      base_classes.concat(" border-b-4")
+    end
+
+    base_classes
+  end
 end
--- spec/helpers/sudoku_helper_spec.rb
+++ spec/helpers/sudoku_helper_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the SudokuHelper. For example:
-#
-# describe SudokuHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe SudokuHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "returns the base classes for non-border indexes" do
+    expect(solution_classes(0)).to eq("w-10 text-2xl text-center border")
+  end
+
+  context "when the index is a right_border index" do
+    it "appends the right border class to the classes list" do
+      expect(solution_classes(2)).to eq("w-10 text-2xl text-center border border-r-4")
+    end
+  end
+
+  context "when the index is a bottom_border index" do
+    it "appends the bottom border class to the classes list" do
+      expect(solution_classes(18)).to eq("w-10 text-2xl text-center border border-b-4")
+    end
+  end
+
+  context "when the index is both a bottom_border and right_border index" do
+    it "appends the bottom border and right border classes to the classes list" do
+      expect(solution_classes(23)).to eq("w-10 text-2xl text-center border border-r-4 border-b-4")
+    end
+  end
 end
